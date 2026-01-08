cb767bd5707c3377ac87d359ecef58535c4e3e34
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-01-08 13:06:03 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import './App.css'
+
+function TituloFormulario (children) {
+  return(
+    <h2>
+    {children}
+    </h2>
+  )
+}
+
+function CampoDeFormulario({children}) {
+  return (
+    <fieldset>
+      {children}
+    </fieldset>
+  )
+}
+
+function Label({children, htmlFor}) {
+  return (
+    <label htmlFo = {htmlFor}>
+      {children}
+    </label>
+  )
+}
+
+function CampoDeEntrada(props) {
+  return <input {...props} />
+}
+
+function FormularioDeEvento () {
+
+  return ( 
+    <form className="form-evento">
+      <TituloFormulario>
+        Preencha para criar um evento:
+      </TituloFormulario>
+
+      <CampoDeFormulario>
+        <Label>
+          Qual o nome do evento?
+        </Label>
+        <CampoDeEntrada
+          type="text" 
+          id='nome' 
+          placeholder='Summer dev hits'
+          name='nomeEvento'
+          />
+      </CampoDeFormulario>
+    </form>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <header>
+        <img src="/logo.png" alt="" />
+      </header>
+      <section>
+        <img src="/banner.png" alt="" />
+      </section>
+      <FormularioDeEvento />
+    </main>
+  )
+}
+
+export default App
