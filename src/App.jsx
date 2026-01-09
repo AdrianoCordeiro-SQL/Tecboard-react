c00cef5854e658a25a04c0e8c835f230d9509f60
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-01-09 09:22:34 -0300

    automatiza a criação de sections no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import './App.css'
-import { Banner } from './componentes/Banner'
-import { FormularioDeEvento } from './componentes/FormularioDeEvento'
-import { Tema } from './componentes/Tema'
+import "./App.css";
+import { Banner } from "./componentes/Banner";
+import { FormularioDeEvento } from "./componentes/FormularioDeEvento";
+import { Tema } from "./componentes/Tema";
 
 function App() {
-
   const temas = [
     {
-      id:1 ,
-      nome: 'front-end'
+      id: 1,
+      nome: "front-end",
     },
     {
-      id:2 ,
-      nome: 'back-end'
+      id: 2,
+      nome: "back-end",
     },
     {
-      id:3 ,
-      nome: 'devops'
+      id: 3,
+      nome: "devops",
     },
     {
-      id:4 ,
-      nome: 'inteligência artificial'
+      id: 4,
+      nome: "inteligência artificial",
     },
     {
-      id:5 ,
-      nome: 'data science'
+      id: 5,
+      nome: "data science",
     },
     {
-      id:6 ,
-      nome: 'cloud'
-    }
-  ]
+      id: 6,
+      nome: "cloud",
+    },
+  ];
   return (
     <main>
       <header>
@@ -38,26 +37,31 @@ function App() {
       </header>
       <Banner />
       <FormularioDeEvento />
-      <section>
-        <Tema tema={temas[0]}/>
+      {temas.map(function (item) {
+        return (
+          <section key={item.id}>
+            <Tema tema={item} />
+          </section>
+        );
+      })}
+
+      {/* <section>
+        <Tema tema={temas[1]} />
       </section>
       <section>
-        <Tema tema={temas[1]}/>
+        <Tema tema={temas[2]} />
       </section>
       <section>
-        <Tema tema={temas[2]}/>
+        <Tema tema={temas[3]} />
       </section>
       <section>
-        <Tema tema={temas[3]}/>
+        <Tema tema={temas[4]} />
       </section>
       <section>
-        <Tema tema={temas[4]}/>
-      </section>
-      <section>
-        <Tema tema={temas[5]}/>
-      </section>
+        <Tema tema={temas[5]} />
+      </section> */}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
